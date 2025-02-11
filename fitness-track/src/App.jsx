372263d2f056639da4489476795185c7fe2409e2
DineshR0066 <ramdinesh2709@gmail.com>
2025-02-11 20:19:14 +0530

    added

--- fitness-track/src/App.jsx
+++ fitness-track/src/App.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import { useState } from "react";
+import "./App.css";
+import NavBar from "./components/NavBar";
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import SearchPage from "./pages/SearchPage";
+import ProfilePage from "./pages/ProfilePage";
 
-const App = () => {
+
+function App() {
+  const [activePageIndex, setActivePageIndex] = useState(0);
+
+  const NAVBAR_ITEMS = [
+    { icon: "🏠", text: "DashBoard", component: <HomePage /> },
+    { icon: "🏋️‍♂️", text: "Workout plan", component: <AboutPage /> },
+    { icon: "🥪", text: "Diet plan", component: <SearchPage /> },
+    { icon: "🙎‍♂️", text: "Profile", component: <ProfilePage /> },
+  ];
+
+  const activePage = NAVBAR_ITEMS[activePageIndex].component;
+
   return (
-    <div>
-      <h1>Dinesh App</h1>
+    <div className="app">
+      <NavBar ITEMS={NAVBAR_ITEMS} setActivePageIndex={setActivePageIndex} />
+      <div className="main-container">
+        {activePage}
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
